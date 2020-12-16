fb85e95ca1023f336d2b12cf92a0608807142a51
Ivan Myrvold <ivan.myrvold@abax.no>
2020-12-16 11:56:11 +0100

    start app from CicdInfraStack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkStack } from '../lib/cdk-stack';
+import { CicdInfraStack } from '../lib/cicd-infra';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new CicdInfraStack(app, 'CicdInfraStack');
+
+app.synth();
