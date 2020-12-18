b26492f81c1c921eaa6e586fc700ef1624548b20
Ivan Myrvold <ivan.myrvold@abax.no>
2020-12-18 16:07:50 +0100

    filter debug

--- UserService/Sources/App/configure.swift
+++ UserService/Sources/App/configure.swift
@@ -14,8 +14,10 @@ public func configure(_ app: Application) throws {
 //    guard let jwksString = Environment.get("JWKS_KEYPAIR") else {
 //        fatalError("No value was found at the given public key environment 'JWKS_KEYPAIR'")
 //    }
-    let jwksString2 = Environment.get("JWKS_KEYPAIR")
+    guard let jwksString2 = Environment.get("JWKS_KEYPAIR") else { fatalError("No value was found at the given public key environment 2 'JWKS_KEYPAIR'") }
     print("configure jwksString2:", jwksString2)
+    guard let jwksString3 = jwksString2.filter { $0 != "\"" } else { fatalError("No value was found at the given public key environment 3 'JWKS_KEYPAIR'") }
+    print("configure jwksString3:", jwksString3)
     try app.jwt.signers.use(jwksJSON: jwksString)
     print("configure 3")
 
